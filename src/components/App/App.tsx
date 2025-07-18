b09551caa37cc1a3732b949d0b367d909b3e4caa
voueva <voueva.e.v@gmail.com>
2025-07-18 15:43:50 +0200

    useQuery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Loader from '../Loader/Loader';
@@ -8,44 +8,46 @@ import SearchBar from '../SearchBar/SearchBar';
 import './App.module.css';
 import type { Movie } from '../../types/movie';
 import { fetchMovies } from '../../services/movieService';
+import { useQuery } from '@tanstack/react-query';
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
+  const [query, setQuery] = useState<string>('');
   const [activeMovie, setActiveMovie] = useState<Movie | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [showError, setShowError] = useState<boolean>(false);
+  const [wasSearched, setWasSearched] = useState<boolean>(false); // новое
 
-  const handleSearch = async (query: string) => {
-    setShowError(false);
-    setIsLoading(true);
-    setMovies([]);
+  const {
+    data: movies = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['movies', query],
+    queryFn: () => fetchMovies(query),
+    enabled: query !== '',
+  });
 
-    try {
-      const newMovieList = await fetchMovies(query);
-
-      if (!newMovieList || newMovieList.length === 0) {
-        toast.error('No movies found for your request.');
-      } else {
-        setMovies(newMovieList);
-      }
-    } catch {
-      setShowError(true);
-      setMovies([]);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = (newQuery: string) => {
+    setQuery(newQuery);
+    setWasSearched(true);
   };
+
+  useEffect(() => {
+    if (wasSearched && !isLoading && movies.length === 0 && !isError) {
+      toast.error('No movies found for your request.');
+    }
+  }, [movies, isLoading, isError, wasSearched]);
 
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
 
       {isLoading && <Loader />}
-      {showError && <ErrorMessage />}
-      {!isLoading && !showError && movies.length > 0 && (
+      {isError && <ErrorMessage />}
+      {!isLoading && !isError && movies.length > 0 && (
         <MovieGrid movies={movies} onSelect={setActiveMovie} />
       )}
-      {activeMovie && <MovieModal movie={activeMovie} onClose={() => setActiveMovie(null)} />}
+      {activeMovie && (
+        <MovieModal movie={activeMovie} onClose={() => setActiveMovie(null)} />
+      )}
 
       <Toaster />
     </>
